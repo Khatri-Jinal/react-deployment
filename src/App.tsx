44736be38abc0d0a1dba5f91f0afe6b48ee4b699
Khatri-Jinal <jinal.khatri@simformsolutions.com>
2022-03-16 11:22:34 +0530

    build generated

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+const NewNews = React.lazy(() => import("./components/News"));
+const NewAbout = React.lazy(() => import("./components/About"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+        </ul>
+        <Suspense fallback={<p>Loading</p>}>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<NewAbout />}></Route>
+            <Route path="/contact" element={<NewNews />}></Route>
+          </Routes>
+        </Suspense>
+      </div>
+    </Router>
   );
 }
 
